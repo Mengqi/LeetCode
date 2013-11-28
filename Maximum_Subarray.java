7d306481a83897a658d0eceb37d644f35ea36ee8
Mengqi Zong <zongmengqi@gmail.com>
2013-11-28 14:19:45 -0800

    maximum subarray o(n)

--- Maximum_Subarray.java
+++ Maximum_Subarray.java
@@ -1,17 +1,27 @@
 public class Solution {
     public int maxSubArray(int[] A) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
         int max = A[0];
+        int sum = 0;
         for (int i = 0; i < A.length; i++) {
-            int sum = 0;
-            for (int j = i; j < A.length; j++) {
-                sum = sum + A[j];
-                if (sum > max) {
-                    max = sum;
+            sum = sum + A[i];
+            if (sum < 0) {
+                sum = 0;
+            }
+            if (sum > max) {
+                max = sum;
+            }
+        }
+        
+        if (max == 0) {
+            // case that all num are negative;
+            max = A[0];
+            for (int i = 0; i < A.length; i++) {
+                if (max < A[i]) {
+                    max = A[i];
                 }
             }
         }
+        
         return max;
     }
 }
