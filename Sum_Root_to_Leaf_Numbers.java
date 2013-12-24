aeececc5fcd6ecd38d5de520d6d86f9d226658c9
Mengqi Zong <zongmengqi@gmail.com>
2013-12-24 00:25:33 -0800

    sum root to leaf numbers

--- Sum_Root_to_Leaf_Numbers.java
+++ Sum_Root_to_Leaf_Numbers.java
@@ -1,44 +1,35 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 public class Solution {
-    public int longestConsecutive(int[] num) {
-        if ((num == null) || (num.length == 0)) {
-            return 0;
+    public int sumNumbers(TreeNode root) {
+        return rootNum(root, 0);
+    }
+    
+    private int rootNum(TreeNode node, int prefix) {
+        if (node == null) {
+            return prefix;
+        }
+        
+        int num = prefix * 10 + node.val;
+        if ((node.left == null) && (node.right == null)) {
+            return num;
         }
 
-        Map<Integer, Integer> numMap = new HashMap<Integer, Integer>();
-        for (int i = 0; i < num.length; i++) {
-            numMap.put(num[i], 0);
+        int sum = 0;
+        if (node.left != null) {
+            sum = sum + rootNum(node.left, num);
         }
-        int start;
-        int end;
-        int length;
-        int maxLength = 1;
-        for (int i = 1; i < num.length; i++) {
-            int flag = numMap.get(num[i]);
-            if (flag == 0) {    // have not visited yet
-                start = num[i];
-                end = num[i];
-                length = 1;
-                boolean startFlag = numMap.containsKey(start-1);
-                boolean endFlag = numMap.containsKey(end+1);
-                while (startFlag || endFlag) {
-                    if (startFlag) {
-                        start = start - 1;
-                        numMap.put(start, 1);
-                        length++;
-                    }
-                    if (endFlag) {
-                        end = end + 1;
-                        numMap.put(end, 1);
-                        length++;
-                    }
-                    if (maxLength < length) {
-                        maxLength = length;
-                    }
-                    startFlag = numMap.containsKey(start-1);
-                    endFlag = numMap.containsKey(end+1);
-                }
-            }
+        if (node.right != null) {
+            sum = sum + rootNum(node.right, num);
         }
-        return maxLength;
+        
+        return sum;
     }
 }
